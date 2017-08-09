7cb704d4f0a6a51d7afb7510a224e1981f6f6f31
wjase <wjase@yahoo.com.au>
2017-08-09 16:59:30 +1000

    Added username to EL context

--- src/main/java/com/cybernostics/themetree/theme/sources/SpringELRequestCondition.java
+++ src/main/java/com/cybernostics/themetree/theme/sources/SpringELRequestCondition.java
@@ -19,6 +19,8 @@ package com.cybernostics.themetree.theme.sources;
  * limitations under the License.
  * #L%
  */
+
+import java.security.Principal;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Optional;
@@ -27,6 +29,7 @@ import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
@@ -41,13 +44,13 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
  *      clientDateTime: a ZonedDateTime representing the time/date of the
  *                      request in the clients time zone.
  *      app: The application context
+ *      username: The current user name or "ANONYMOUS" if noone logged in
  *      request: The current HttpServletRequest
  * </pre>
  *
  * @author jason
  */
-public class SpringELRequestCondition implements Function<HttpServletRequest, Boolean>
-{
+public class SpringELRequestCondition implements Function<HttpServletRequest, Boolean> {
 
     private static ExpressionParser parser = new SpelExpressionParser();
     private Expression conditionalExpression;
@@ -55,23 +58,19 @@ public class SpringELRequestCondition implements Function<HttpServletRequest, Bo
     private Optional<BiConsumer<StandardEvaluationContext, HttpServletRequest>> requestSimpleVariableExtractor = Optional.empty();
     private static Logger log = Logger.getLogger(SpringELRequestCondition.class.getName());
 
-    public SpringELRequestCondition()
-    {
+    public SpringELRequestCondition() {
     }
 
-    public SpringELRequestCondition(String expression)
-    {
+    public SpringELRequestCondition(String expression) {
         conditionalExpression = parser.parseExpression(expression);
         evalContext.setRootObject(this);
     }
 
-    public void addMethod(String methodName, Function<Object[], Object> lambda)
-    {
+    public void addMethod(String methodName, Function<Object[], Object> lambda) {
         LambdaExpressionMethod.onContext(evalContext).registerMethod(methodName, lambda);
     }
 
-    public void setApplicationContext(ApplicationContext context)
-    {
+    public void setApplicationContext(ApplicationContext context) {
         evalContext.setVariable("app", context);
     }
 
@@ -83,24 +82,22 @@ public class SpringELRequestCondition implements Function<HttpServletRequest, Bo
      *
      * @param requestSimpleVaribaleExtractor
      */
-    public void setRequestSimpleVariableExtractor(BiConsumer<StandardEvaluationContext, HttpServletRequest> requestSimpleVaribaleExtractor)
-    {
+    public void setRequestSimpleVariableExtractor(BiConsumer<StandardEvaluationContext, HttpServletRequest> requestSimpleVaribaleExtractor) {
         this.requestSimpleVariableExtractor = Optional.of(requestSimpleVaribaleExtractor);
     }
 
     @Override
-    public Boolean apply(HttpServletRequest t)
-    {
+    public Boolean apply(HttpServletRequest t) {
         evalContext.setVariable("request", t);
+        Principal principal = t.getUserPrincipal();
+        evalContext.setVariable("username", principal == null ? "ANONYMOUS" : principal.getName());
+
         final String requestDateString = t.getHeader("Date");
-        if (requestDateString != null)
-        {
-            try
-            {
+        if (requestDateString != null) {
+            try {
                 ZonedDateTime requestDate = ZonedDateTime.parse(requestDateString, DateTimeFormatter.RFC_1123_DATE_TIME);
                 evalContext.setVariable("clientDateTime", requestDate);
-            } catch (Throwable th)
-            {
+            } catch (Throwable th) {
                 log.log(Level.SEVERE, "Bad request date from http request header", th);
                 throw th;
             }
