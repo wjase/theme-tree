9fc8e0a79201f49ad26359d419a5eb46fb89e673
wjase <wjase@yahoo.com.au>
2017-09-06 06:23:37 +1000

    Made static resource path and locations use properties

--- src/main/java/au/com/cybernostics/themetree/config/StaticResourcesConfiguration.java
+++ src/main/java/au/com/cybernostics/themetree/config/StaticResourcesConfiguration.java
@@ -20,22 +20,39 @@ package au.com.cybernostics.themetree.config;
  * #L%
  */
 import au.com.cybernostics.themetree.asset.StaticResourceRootCollection;
+import static java.util.Arrays.asList;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * <p>
+ * StaticResourcesConfiguration class.</p>
  *
  * @author jason wraxall
+ * @version $Id: $Id
  */
 @Configuration
-public class StaticResourcesConfiguration
-{
+public class StaticResourcesConfiguration {
 
+    @Value("${static.resource.prefixes:css,js,img,font}")
+    String[] resourcePrefixes;
+
+    @Value("${static.resource.locations:classpath:/templates/,classpath:/static/,classpath:/webroot/}")
+    String[] resourceLocations;
+
+    /**
+     * <p>
+     * resourceRootCollection.</p>
+     *
+     * @return a
+     * {@link au.com.cybernostics.themetree.asset.StaticResourceRootCollection}
+     * object.
+     */
     @Bean
     @ConditionalOnMissingBean(StaticResourceRootCollection.class)
-    public StaticResourceRootCollection resourceRootCollection()
-    {
-        return new StaticResourceRootCollection("css", "js", "img", "font");
+    public StaticResourceRootCollection resourceRootCollection() {
+        return new StaticResourceRootCollection(asList(resourcePrefixes), asList(resourceLocations));
     }
 }
